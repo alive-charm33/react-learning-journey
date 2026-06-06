6bc4cab4524d77e7dd91429860e0a6e56332114b
bargalesejal@gmail.com <bargalesejal@gmail.com>
2026-06-06 15:18:10 +0530

    Day 2: Added Navbar and Card components

--- 02_components/02_components/src/App.jsx
+++ 02_components/02_components/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import Card from './components/Card'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <h1>hi I am Sejal inside App component</h1>
+      <Navbar />
+      <Navbar />
+      <Card />
+      <Card />
+    </>
   )
 }
 
---
+++ 02_components/02_components/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      This is navbar
+    </div>
+  )
+}
+export default Navbar;
