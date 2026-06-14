8fb8b1785d861b3b6aecd89ffd4a50dc967efa66
bargalesejal@gmail.com <bargalesejal@gmail.com>
2026-06-14 17:19:42 +0530

    08_functions functions in react

---
+++ 08_functions/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+  function btnclicked(){
+    console.log('button is clicked')
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
