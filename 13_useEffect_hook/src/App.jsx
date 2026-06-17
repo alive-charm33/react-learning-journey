a194c1715aeb2e22bdf912b5fae75d5ee202045f
bargalesejal@gmail.com <bargalesejal@gmail.com>
2026-06-17 11:03:20 +0530

    used useEffect hook for learning

---
+++ 13_useEffect_hook/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default App;
